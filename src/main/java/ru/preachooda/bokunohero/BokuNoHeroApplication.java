9775346195a70447d9cf7713b5ca81ca6777decf
Kirill Bobryakov <kirillbobryakov82@gmail.com>
2023-06-20 16:53:39 +0800

    init

---
+++ src/main/java/ru/preachooda/bokunohero/BokuNoHeroApplication.java
@@ -0,0 +1,13 @@
+package ru.preachooda.bokunohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokuNoHeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokuNoHeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/preachooda/bokunohero/BokuNoHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.preachooda.bokunohero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokuNoHeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
