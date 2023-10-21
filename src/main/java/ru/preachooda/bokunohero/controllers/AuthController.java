adff1e76e5f278c25c03d9aeea0f2138971b65f6
Kirill Bobryakov <kirillbobryakov82@gmail.com>
2023-10-21 14:20:59 +0300

    feature(hero-service): временное отключение аутентификации и авторизации

--- src/main/java/ru/preachooda/bokunohero/controllers/AuthController.java
+++ src/main/java/ru/preachooda/bokunohero/controllers/AuthController.java
@@ -1,49 +1,49 @@
-package ru.preachooda.bokunohero.controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.preachooda.bokunohero.dto.AuthResponse;
-import ru.preachooda.bokunohero.dto.UserDto;
-import ru.preachooda.bokunohero.entity.User;
-import ru.preachooda.bokunohero.entity.Role;
-import ru.preachooda.bokunohero.services.AuthService;
-import ru.preachooda.bokunohero.services.UserService;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
-
-    @Autowired
-    private AuthService authService;
-
-    @Autowired
-    private UserService userService;
-
-    @PostMapping("/register")
-    public void register(@RequestBody UserDto userDto) {
-        this.authService.register(userDto);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody UserDto userDto) {
-        String token = authService.login(userDto);
-        User user = userService.findByUsername(userDto.getUsername());
-        String fullName = user.getLabel();
-        List<String> roles = user.getRoles().stream().map(Role::getCode).collect(Collectors.toList());
-        AuthResponse authResponse = AuthResponse.builder()
-                .authenticationToken(token)
-                .username(userDto.getUsername())
-                .fullName(fullName)
-                .roles(roles)
-                .build();
-        return ResponseEntity.ok().body(authResponse);
-    }
-
-}
+//package ru.preachooda.bokunohero.controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import ru.preachooda.bokunohero.dto.AuthResponse;
+//import ru.preachooda.bokunohero.dto.UserDto;
+//import ru.preachooda.bokunohero.entity.User;
+//import ru.preachooda.bokunohero.entity.Role;
+//import ru.preachooda.bokunohero.services.AuthService;
+//import ru.preachooda.bokunohero.services.UserService;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@RestController
+//@RequestMapping("/auth")
+//public class AuthController {
+//
+//    @Autowired
+//    private AuthService authService;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @PostMapping("/register")
+//    public void register(@RequestBody UserDto userDto) {
+//        this.authService.register(userDto);
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<AuthResponse> login(@RequestBody UserDto userDto) {
+//        String token = authService.login(userDto);
+//        User user = userService.findByUsername(userDto.getUsername());
+//        String fullName = user.getLabel();
+//        List<String> roles = user.getRoles().stream().map(Role::getCode).collect(Collectors.toList());
+//        AuthResponse authResponse = AuthResponse.builder()
+//                .authenticationToken(token)
+//                .username(userDto.getUsername())
+//                .fullName(fullName)
+//                .roles(roles)
+//                .build();
+//        return ResponseEntity.ok().body(authResponse);
+//    }
+//
+//}
--- src/main/java/ru/preachooda/bokunohero/services/AuthService.java
+++ src/main/java/ru/preachooda/bokunohero/services/AuthService.java
@@ -1,81 +1,81 @@
-package ru.preachooda.bokunohero.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import ru.preachooda.bokunohero.dto.UserDto;
-import ru.preachooda.bokunohero.entity.Role;
-import ru.preachooda.bokunohero.entity.User;
-import ru.preachooda.bokunohero.repository.RoleRepository;
-import ru.preachooda.bokunohero.repository.UserRepository;
-import ru.preachooda.bokunohero.security.JwtProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class AuthService {
-
-    @Autowired
-    private final PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private final RoleRepository roleRepository;
-
-    @Autowired
-    private final UserRepository userRepository;
-
-    @Autowired
-    private final AuthenticationManager authenticationManager;
-
-    @Autowired
-    private final JwtProvider jwtProvider;
-
-    @Autowired
-    public AuthService(PasswordEncoder passwordEncoder, RoleRepository roleRepository,
-                       UserRepository userRepository, AuthenticationManager authenticationManager,
-                       JwtProvider jwtProvider)
-    {
-        this.passwordEncoder = passwordEncoder;
-        this.roleRepository = roleRepository;
-        this.userRepository = userRepository;
-        this.authenticationManager = authenticationManager;
-        this.jwtProvider = jwtProvider;
-    }
-
-    public void register(UserDto userDto) {
-        User user = new User();
-        List<Role> roles = new ArrayList<>();
-
-        user.setUsername(userDto.getUsername());
-        user.setPassword(encodePassword(userDto.getPassword()));
-        userDto.getRoles().forEach(r -> roles.add(roleRepository.findByCode(r.getCode())));
-        user.setRoles(roles);
-
-        userRepository.save(user);
-    }
-
-    public String login(UserDto userDto) {
-        Authentication authenticate;
-
-        try {
-            authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDto.getUsername(),
-                    userDto.getPassword()));
-        } catch (AuthenticationException authenticationException) {
-            return "";
-        }
-
-        assert authenticate != null;
-
-        return jwtProvider.generateToken(authenticate.getName());
-    }
-
-    private String encodePassword(String password) {
-        return passwordEncoder.encode(password);
-    }
-
-}
+//package ru.preachooda.bokunohero.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import ru.preachooda.bokunohero.dto.UserDto;
+//import ru.preachooda.bokunohero.entity.Role;
+//import ru.preachooda.bokunohero.entity.User;
+//import ru.preachooda.bokunohero.repository.RoleRepository;
+//import ru.preachooda.bokunohero.repository.UserRepository;
+//import ru.preachooda.bokunohero.security.JwtProvider;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class AuthService {
+//
+//    @Autowired
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private final RoleRepository roleRepository;
+//
+//    @Autowired
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    private final AuthenticationManager authenticationManager;
+//
+//    @Autowired
+//    private final JwtProvider jwtProvider;
+//
+//    @Autowired
+//    public AuthService(PasswordEncoder passwordEncoder, RoleRepository roleRepository,
+//                       UserRepository userRepository, AuthenticationManager authenticationManager,
+//                       JwtProvider jwtProvider)
+//    {
+//        this.passwordEncoder = passwordEncoder;
+//        this.roleRepository = roleRepository;
+//        this.userRepository = userRepository;
+//        this.authenticationManager = authenticationManager;
+//        this.jwtProvider = jwtProvider;
+//    }
+//
+//    public void register(UserDto userDto) {
+//        User user = new User();
+//        List<Role> roles = new ArrayList<>();
+//
+//        user.setUsername(userDto.getUsername());
+//        user.setPassword(encodePassword(userDto.getPassword()));
+//        userDto.getRoles().forEach(r -> roles.add(roleRepository.findByCode(r.getCode())));
+//        user.setRoles(roles);
+//
+//        userRepository.save(user);
+//    }
+//
+//    public String login(UserDto userDto) {
+//        Authentication authenticate;
+//
+//        try {
+//            authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDto.getUsername(),
+//                    userDto.getPassword()));
+//        } catch (AuthenticationException authenticationException) {
+//            return "";
+//        }
+//
+//        assert authenticate != null;
+//
+//        return jwtProvider.generateToken(authenticate.getName());
+//    }
+//
+//    private String encodePassword(String password) {
+//        return passwordEncoder.encode(password);
+//    }
+//
+//}
